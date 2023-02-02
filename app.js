966114494fc81f7f8683fd035d4695beee243e6f
Josh <josh.petersen@publicisna.com>
2023-02-02 12:01:54 -0800

    setting up js file

---
+++ app.js
@@ -0,0 +1,22 @@
+const callyApp = {};
+
+// start: Mobile
+callyApp.mobile = () => {
+    console.log("lets get mobile")
+}
+// end: Mobile
+
+
+
+// start: Loader
+callyApp.loader = () => {
+    console.log("Loading, Please Hold")
+}
+// end: Loader
+
+
+callyApp.init = () => {
+    console.log("It's Alive")
+}
+
+callyApp.init();
