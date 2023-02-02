e0a0b58f15563625075b0306c99ff01051a0af74
Josh <josh.petersen@publicisna.com>
2023-02-02 14:50:25 -0800

    created nav with mobile responsiveness and js logic

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const callyApp = {};
 
 // start: Mobile
 callyApp.mobile = () => {
-    console.log("lets get mobile")
+    console.log("lets get mobile");
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".navMenu");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle('active')
+        navMenu.classList.toggle('active')
+    });
+
+    document.querySelectorAll(".navLink").forEach(n => n.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }))
 }
 // end: Mobile
 
@@ -16,7 +28,9 @@ callyApp.loader = () => {
 
 
 callyApp.init = () => {
-    console.log("It's Alive")
+    console.log("It's Alive");
+
+    callyApp.mobile();
 }
 
 callyApp.init();
